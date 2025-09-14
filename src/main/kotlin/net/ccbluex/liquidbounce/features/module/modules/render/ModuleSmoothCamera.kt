a71167ef06975d0db869a49ef5f26c6868a3a48d
LanlanMC <Langrowup@outlook.com>
2025-09-14 14:29:10 +0800

    improve(SmoothCamera): improved visuals

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleSmoothCamera.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/render/ModuleSmoothCamera.kt
@@ -22,7 +22,6 @@ package net.ccbluex.liquidbounce.features.module.modules.render
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.ClientModule
 import net.ccbluex.liquidbounce.utils.math.isLikelyZero
-import net.minecraft.util.math.MathHelper
 import net.minecraft.util.math.Vec3d
 
 /**
@@ -32,36 +31,49 @@ import net.minecraft.util.math.Vec3d
  */
 object ModuleSmoothCamera : ClientModule("SmoothCamera", Category.RENDER) {
 
-    private val factor by float("Factor", 0.2f, 0.0f..1.0f)
+    private val enableFirstPOV by boolean("Enable First Pov", default = false)
+    private val resetOnPerspectiveChange by boolean("Reset on Perspective Change", default = true)
+
+    private val factorH by float("Horizontal Factor", 0.9f, 0.0f..1.0f)
+    private val factorV by float("Vertical Factor", 0.93f, 0.0f..1.0f)
 
     var smoothPos: Vec3d = Vec3d.ZERO
         private set
-    var smoothYaw = 0f
-        private set
-    var smoothPitch = 0f
-        private set
+
+    private var lastPerspective = mc.options.perspective
 
     override fun onDisabled() {
         smoothPos = Vec3d.ZERO
-        smoothYaw = 0f
-        smoothPitch = 0f
     }
 
     @JvmStatic
-    fun cameraUpdate(yaw: Float, pitch: Float, pos: Vec3d) {
-        if (!running) return
+    fun cameraUpdate(pos: Vec3d) {
+        if (!running) {
+            lastPerspective = mc.options.perspective
+            return
+        }
+        // This provides better responsiveness when switching perspectives
+        if (resetOnPerspectiveChange && lastPerspective != mc.options.perspective) {
+            smoothPos = pos
+            lastPerspective = mc.options.perspective
+            return
+        }
+        lastPerspective = mc.options.perspective
+        // Don't smooth for first person because it looks weird
+        if (!enableFirstPOV && mc.options.perspective.isFirstPerson) {
+            smoothPos = pos
+            return
+        }
 
         if (smoothPos.isLikelyZero) {
             smoothPos = pos
-            smoothYaw = yaw
-            smoothPitch = pitch
         }
 
-        val eased = factor
-
-        smoothPos = smoothPos.lerp(pos, eased.toDouble())
-        smoothYaw += MathHelper.wrapDegrees(yaw - smoothYaw) * eased
-        smoothPitch += (pitch - smoothPitch) * eased
+        smoothPos = Vec3d(
+            smoothPos.x * factorH + pos.x * (1 - factorH),
+            smoothPos.y * factorV + pos.y * (1 - factorV),
+            smoothPos.z * factorH + pos.z * (1 - factorH)
+        )
     }
 
     @JvmStatic
